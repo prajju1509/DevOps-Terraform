090ae45130f89b4a2d7083e0373842be8c0db789
Prajwal BM <117504878+prajju1509@users.noreply.github.com>
2022-12-20 14:09:39 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -9,11 +9,11 @@ resource "aws_instance" "web1" {
   }
 }
 
-resource "aws_instance" "web2" {
-  ami           = "ami-06cccb48f83164ce7"
-  instance_type = "t2.micro"
+#resource "aws_instance" "web2" {
+ # ami           = "ami-06cccb48f83164ce7"
+  #instance_type = "t2.micro"
 
-  tags = {
-    Name = "Instance-2"
-  }
-}
+  #tags = {
+   # Name = "Instance-2"
+  #}
+#}
